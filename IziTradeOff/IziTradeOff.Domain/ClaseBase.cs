1a5308f450861617ea394a78ff2a1277413ad62c
Johnny Arcia <cosmicvanbuuren@hotmail.com>
2022-10-20 22:25:16 -0600

    Base

---
+++ IziTradeOff/IziTradeOff.Domain/ClaseBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IziTradeOff.Domain
+{
+    public class ClaseBase
+    {
+        /// <summary>
+        /// Id del modelo
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Estado Activo-Inactivo
+        /// </summary>
+        public bool Estado { get; set; }
+    }
+}
---
+++ IziTradeOff/IziTradeOff.Domain/Usuario.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IziTradeOff.Domain
+{
+    public class Usuario : IdentityUser
+    {
+        public string NombreCompleto { get; set; }
+        public bool Activo { get; set; }
+    }
+}
